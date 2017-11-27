19226ea95df83f7de9c541e096bfc605468779ae
uccu <418667631@qq.com>
2017-11-27 23:53:18 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2 v-on:click="te()">Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="#/login">vue-router</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import $ from 'jquery'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    te () {
+      console.log($)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+body{
+  background:#fff
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tc login container">
+    <h1>{{msg}}</h1>
+
+    <form class="form" method="post" action="#/dd">
+      <label></label>
+      <input name="email">
+      <input name="password" type="password">
+      
+
+    </form>
+    <button v-on:click="te()">11</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'Login'
+    }
+  },
+  methods: {
+    te () {
+      this.msg = 'ww'
+      console.log(this.msg)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
